6175100e11ecad0e6a17bc738fe26218d1ee65d8
abdulkareem1094 <abdulak4t7@gmail.com>
2023-10-05 15:23:54 -0700

    api

---
+++ api/index.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+
+
+const app = express();
+const port = 8000;
+const cors = require('cors');
+app.use(cors());
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(bodyParser.json());
+
+const jwt = require('jsonwebtoken');
+
+
+mongoose.connect('mongodb+srv://Clon3:walid1402@cluster0.cqcm7ge.mongodb.net/')
